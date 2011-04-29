fc88ef33140320295948c3818dfded016a048708
swcool <ac2k888@yahoo.com>
2011-04-29 02:16:06 -0700

    update strategy

--- oa-oauth/lib/omniauth/strategies/weibo.rb
+++ oa-oauth/lib/omniauth/strategies/weibo.rb
@@ -4,26 +4,24 @@ require 'multi_json'
 module OmniAuth
   module Strategies
     #
-    # Authenticate to Sina WeiBo via OAuth and retrieve basic
+    # Authenticate to weibo via OAuth and retrieve basic
     # user information.
     #
     # Usage:
     #
-    #    use OmniAuth::Strategies::Weibo, 'consumerkey', 'consumersecret'
+    #    use OmniAuth::Strategies::weibo, 'APIKey', 'APIKeySecret'
     #
     class Weibo < OmniAuth::Strategies::OAuth
-      # Initialize the middleware
-      #
-      # @option options [Boolean, true] :sign_in When true, use the "Sign in with Twitter" flow instead of the authorization flow.
+
       def initialize(app, consumer_key = nil, consumer_secret = nil, options = {}, &block)
         @api_key = consumer_key
 
         client_options = {
-          :site 		=> 'http://api.t.sina.com.cn',
-	  :request_token_path 	=> '/oauth/request_token',
-    	  :access_token_path 	=> '/oauth/access_token',
-          :authorize_path    	=> '/oauth/authorize',
-          :realm         	=> 'OmniAuth'
+          :site               => 'http://api.t.sina.com.cn',
+          :request_token_path => '/oauth/request_token',
+          :access_token_path  => '/oauth/access_token',
+          :authorize_path     => '/oauth/authorize',
+          :realm              => 'OmniAuth'
         }
 
         super(app, :weibo, consumer_key, consumer_secret, client_options, options, &block)
@@ -39,21 +37,19 @@ module OmniAuth
 
       def user_info
         user_hash = self.user_hash
-
         {
-          
-	  'username' => user_hash['screen_name'],
-	  'name' => user_hash['name'],    
+          'username' => user_hash['screen_name'],
+          'name' => user_hash['name'],
           'location' => user_hash['location'],
-          'description' => user_hash['description'],
           'image' => user_hash['profile_image_url'],
+          'description' => user_hash['description'],
           'urls' => {
-		'Weibo' => user_hash['url']
-	   }
+            'Weibo' => user_hash['url']
+          }
         }
       end
 
-      # MonkeyPatch session['oath']['weibo']['callback_confirmed'] to true
+      # MonkeyPatch session['oauth']['weiboWeibo']['callback_confirmed'] to true
       def request_phase
         request_token = consumer.get_request_token(:oauth_callback => callback_url)
         session['oauth'] ||= {}
@@ -72,6 +68,9 @@ module OmniAuth
       end
 
       def user_hash
+        # http://api.t.sina.com.cn/users/show/:id.json?source=appkey
+        # @access_token.params[:user_id] is the UID
+        # @api_key is the appkey
         uid = @access_token.params[:user_id]
         @user_hash ||= MultiJson.decode(@access_token.get("http://api.t.sina.com.cn/users/show/#{uid}.json?source=#{@api_key}").body)
       end
