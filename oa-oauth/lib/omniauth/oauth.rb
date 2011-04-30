49c5bac24d4b24d7e139d4b176a7d4d3d7f43a98
swcool <ac2k888@yahoo.com>
2011-04-29 20:10:41 -0700

    debug

--- oa-oauth/lib/omniauth/oauth.rb
+++ oa-oauth/lib/omniauth/oauth.rb
@@ -46,6 +46,7 @@ module OmniAuth
     autoload :TypePad,            'omniauth/strategies/type_pad'
     autoload :Vimeo,              'omniauth/strategies/vimeo'
     autoload :Vkontakte,          'omniauth/strategies/vkontakte'
+    autoload :Wwitter,            'omniauth/strategies/wwitter'
     autoload :Weibo,              'omniauth/strategies/weibo'
     autoload :Yahoo,              'omniauth/strategies/yahoo'
     autoload :Yammer,             'omniauth/strategies/yammer'
